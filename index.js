6737e15c898a436437a9527a16d7a6731ae28d08
SamuelWango <wangos478@gmail.com>
2023-06-17 11:51:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+const input=document.getElementById('button')
+
+function clickAlert(){
+    alert('I was clicked!')
 }
+
+function addingEventListener() {
+    input.addEventListener('click', clickAlert)
+}
+
+addingEventListener()
